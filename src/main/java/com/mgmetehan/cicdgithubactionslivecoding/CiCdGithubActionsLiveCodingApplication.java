f467c36c78132c1346fc59f3853b6467834b0507
mgmetehan <mgmetehan@hotmail.com>
2023-11-07 21:43:22 +0300

    init project

---
+++ src/main/java/com/mgmetehan/cicdgithubactionslivecoding/CiCdGithubActionsLiveCodingApplication.java
@@ -0,0 +1,13 @@
+package com.mgmetehan.cicdgithubactionslivecoding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CiCdGithubActionsLiveCodingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CiCdGithubActionsLiveCodingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mgmetehan/cicdgithubactionslivecoding/CiCdGithubActionsLiveCodingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mgmetehan.cicdgithubactionslivecoding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CiCdGithubActionsLiveCodingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
